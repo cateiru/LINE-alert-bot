2ae7ba5f250a1f4b2bb19db31e88aa3f59d83da0
Yuto Watanabe <yuto.w51942@gmail.com>
2020-04-22 17:05:29 +0900

    refactoring

--- src/template.py
+++ src/template.py
@@ -6,14 +6,25 @@ from typing import Any
 
 
 def apply_template(text: Any) -> Any:
+    '''
+    タイトルから適切なテンプレートを指定します。
+
+    Args:
+        text (Any): 送信するタイトルなどの情報が入ったDict
+
+    Returns:
+        Any: テンプレートを適用
+    '''
     title = text['title']
     tsunami = re.search(r'津波', title)
+    seismic_intensity_bulletin = re.search(r'震度速報', title)
+    epicenter_and_seismic_intensity = re.search(r'震源・震度に関する情報', title)
 
-    if title == '震度速報':
+    if seismic_intensity_bulletin:
         template = seismic_intensity_bulletin_template(text)
     elif title == '震源に関する情報':
         template = epicenter_information_template(text)
-    elif title == '震源・震度に関する情報':
+    elif epicenter_and_seismic_intensity:
         template = epicenter_and_seismic_intensity_template(text)
     elif title == '緊急地震速報（予報）':
         template = earthquake_early_warning_forecast_template(text)
@@ -53,6 +64,13 @@ def apply_template(text: Any) -> Any:
 def seismic_intensity_bulletin_template(text: Any) -> Any:
     '''
     震度速報のテンプレートを設定する。
+
+    Args:
+        text (Any): 送信するタイトルなどの情報が入ったDict
+                    title, body, areas, info
+
+    Returns:
+        Any: テンプレートを適用
     '''
     title = text['title']
     body = text['body']
@@ -139,6 +157,13 @@ def seismic_intensity_bulletin_template(text: Any) -> Any:
 def epicenter_information_template(text: Any) -> Any:
     '''
     震源に関する情報のテンプレートを設定する。
+
+    Args:
+        text (Any): 送信するタイトルなどの情報が入ったDict
+                    title, body, magnitude, area, info
+
+    Returns:
+        Any: テンプレートを適用
     '''
     title = text['title']
     body = text['body']
@@ -232,6 +257,13 @@ def epicenter_information_template(text: Any) -> Any:
 def epicenter_and_seismic_intensity_template(text: Any) -> Any:
     '''
     震源・震度に関する情報のテンプレートを設定する。
+
+    Args:
+        text (Any): 送信するタイトルなどの情報が入ったDict
+                    title, body, magnitude, area, max_seismic_intensity, info
+
+    Returns:
+        Any: テンプレートを適用
     '''
     title = text['title']
     body = text['body']
@@ -340,6 +372,13 @@ def epicenter_and_seismic_intensity_template(text: Any) -> Any:
 def earthquake_early_warning_forecast_template(text: Any) -> Any:
     '''
     緊急地震速報(予報)のテンプレートを設定する。
+
+    Args:
+        text (Any): 送信するタイトルなどの情報が入ったDict
+                    title, body
+
+    Returns:
+        Any: テンプレートを適用
     '''
     title = text['title']
     body = text['body']
@@ -389,6 +428,13 @@ def earthquake_early_warning_forecast_template(text: Any) -> Any:
 def earthquake_early_warning_alarm_template(text: Any) -> Any:
     '''
     緊急地震速報(警報)のテンプレートを設定する。
+
+    Args:
+        text (Any): 送信するタイトルなどの情報が入ったDict
+                    title, body
+
+    Returns:
+        Any: テンプレートを適用
     '''
     title = text['title']
     body = text['body']
@@ -452,6 +498,13 @@ def earthquake_early_warning_alarm_template(text: Any) -> Any:
 def tsunami_template(text: Any) -> Any:
     '''
     津波情報のテンプレートを設定する。
+
+    Args:
+        text (Any): 送信するタイトルなどの情報が入ったDict
+                    title, body, (area)
+
+    Returns:
+        Any: テンプレートを適用
     '''
     title = text['title']
     body = text['body']
